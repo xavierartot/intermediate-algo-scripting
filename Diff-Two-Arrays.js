cdfb997a88cdb6c309e8ee2bf5204714fe200def
xavierartot <xavierartot@gmail.com>
2016-08-24 11:10:51 -0500

    try the idea with sort

--- Diff-Two-Arrays.js
+++ Diff-Two-Arrays.js
@@ -15,37 +15,68 @@
 // fruits vaut --> ["Banane", "Orange", "Citron", "Pomme", "Mangue"]
 // agrumes vaut --> ["Orange", "Citron"]
 //
-var indices = [];
-var tableau = ['a', 'b', 'a', 'c', 'a', 'd'];
-var element = 'c';
-var idx = tableau.indexOf(element);
-while (idx !== -1) {
-  indices.push(idx);
-  idx = tableau.indexOf(element, idx + 1);
-}
-console.log(indices, idx);
+//var indices = [];
+//var tableau = ['a', 'b', 'a', 'c', 'a', 'd'];
+//var element = 'c';
+//var idx = tableau.indexOf(element);
+//while (idx !== -1) {
+  //indices.push(idx);
+  //idx = tableau.indexOf(element, idx + 1);
+//}
+//console.log(indices, idx);
 // [0, 2, 4]/
 (function() {
   function diffArray(arr1, arr2) {
-    var ele,indices, idx, arr;
+    //replace
+    //var str = "Visit, Microsoft!";
+    //var res = str.replace("Microsoft", "");
+    //console.log(res);
+
+    //second idea, I join the array to a string then use a regex to test 
+    //the string in a loop.
+    var ele,indices, idx, arr, rgx, arr4, textJoin;
     var newArr = [];
-    if (arr1.length <= arr2.length) {
-      console.log("arr2 est plus grand ou ==");
-      for (var i = 0, len = arr1.length; i < len; i++) {
-        idx = arr2.indexOf(arr1[i]);
-        if (idx ===  -1) {// ne trouve pas dans la le tableau
-          newArr.push(arr1[i]);
-        } else {
-        }
-      }
-    } else {
-      console.log("arr1 est plus grand");
+    //console.log(string.indexOf(substring) !== -1);
+    //couper la chaine
+    //rgx = text.replace('dead', "");
+    //console.log(rgx);
+    //
+    function compareNombres(a, b) {
+      return a - b;
     }
 
-    console.log(newArr);
+    arr = arr1.concat(arr2);
+    console.log(arr);
+    var arrayString = arr.sort(compareNombres );
+    console.log(arrayString );
 
-    // Same, same; but different.
-    return newArr;
+    arrayString =  textJoin.sort();
+    var string = "foo",
+    substring = "a";
+    console.log("Triées : " +  textJoin.sort() +"\n\n");
+
+    for (var i = 0, len = arr.length; i < len; i++) {
+      //console.log(text.indexOf(arr[i]) !== -1);
+      //si arr[i] match the chaine, arrayString 
+      if (text.indexOf(arr[i]) !== -1) {
+        //couper la chaine
+      }
+      ele = text.replace(arr[i], "xxxxxxxxxxxxxx");
+      //console.log(ele);
+      //si arr[i] match the chaine, arrayString 
+        //couper la chaine
+        //rgx = text.replace(arr[i], "");
+        //si arr[i] mathe the chaine, arrayString 
+          //couper encore
+          //rgx = text.replace(arr[i], "");
+    }
+    //console.log(text );
+
+    //split() arrayString in an array with the comma
+    
+    //console.log(newArr);
+
+    //return newArr;
   }
 
   //diffArray(
